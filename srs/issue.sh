daeb7a3e34d32f0ff7155be721fe615c03fa43c7
winlin <winlin@vip.126.com>
2023-07-26 12:37:45 +0800

    Refine files.

---
+++ srs/issue.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Execute by: bash xxx.sh or bash zzz/yyy/xxx.sh or ./xxx.sh or ./zzz/yyy/xxx.sh source xxx.sh
+REALPATH=$(realpath ${BASH_SOURCE[0]})
+SCRIPT_DIR=$(cd $(dirname ${REALPATH}) && pwd)
+WORK_DIR=$(cd $(dirname ${REALPATH})/.. && pwd)
+echo "BASH_SOURCE=${BASH_SOURCE}, REALPATH=${REALPATH}, SCRIPT_DIR=${SCRIPT_DIR}, WORK_DIR=${WORK_DIR}"
+cd ${WORK_DIR}
+
+source venv/bin/activate
+python issue-trans.py $@
