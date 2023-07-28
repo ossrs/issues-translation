d0062888c347f018be3dcb39949b7fc76efb00ea
winlin <winlin@vip.126.com>
2023-07-28 18:34:33 +0800

    Ignore GitHub error.

--- tools.py
+++ tools.py
@@ -194,7 +194,13 @@ class GithubGraphQLException(Exception):
     def is_forbidden(self):
         if self.errors is not None and len(self.errors) > 0:
             for error in self.errors:
-                if 'type' in error and error['type'] == 'FORBIDDEN':
+                if 'type' not in error:
+                    continue
+                # For example: Resource not accessible by personal access token
+                if error['type'] == 'FORBIDDEN':
+                    return True
+                # For example: Cannot be modified since the issue has been converted to a discussion.
+                if error['type'] == 'UNPROCESSABLE':
                     return True
         return False
 
