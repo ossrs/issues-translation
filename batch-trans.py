fa08ecab886334fb4de9faf3a4aed6c9c8db4857
winlin <winlin@vip.126.com>
2023-07-29 15:03:31 +0800

    Refine code.

--- batch-trans.py
+++ batch-trans.py
@@ -8,7 +8,7 @@ parser.add_argument("--input", type=str, required=True, help="GitHub repository 
 parser.add_argument("--token", type=str, required=False, help="GitHub access token, for example, github_pat_xxx_yyyyyy")
 parser.add_argument("--proxy", type=str, required=False, help="OpenAI API proxy, for example, x.y.z")
 parser.add_argument("--key", type=str, required=False, help="OpenAI API key, for example, xxxyyyzzz")
-parser.add_argument("--isf", type=str, default='issue', required=False, help="The filter can be [issue, pr, pullrequest, disscussion], for example, issue")
+parser.add_argument("--isf", type=str, default='issue', required=False, help="The filter can be [issue, pr, pullrequest, discussion], for example, issue")
 parser.add_argument("--count", type=int, default=10, required=False, help="The count of issues, for example, 10")
 
 args = parser.parse_args()
@@ -21,7 +21,7 @@ if 'is:' not in isf:
 
 if 'issue' in args.isf:
     script = "srs/issue.sh"
-elif 'pr' or 'pullrequest' in args.isf:
+elif 'pr' in args.isf or 'pullrequest' in args.isf:
     script = "srs/pr.sh"
 elif 'discussion' in args.isf:
     script = "srs/discussion.sh"
@@ -35,6 +35,7 @@ logs.append(f"proxy: {len(openai.api_base)}B")
 logs.append(f"key: {len(openai.api_key)}B")
 logs.append(f"isf: {isf}")
 logs.append(f"count: {args.count}")
+logs.append(f"script: {script}")
 print(f"run with {', '.join(logs)}")
 
 if args.count <= 0 or args.count > 100:
@@ -53,11 +54,11 @@ j_issues = tools.search_issues(
 comments = 0
 for j_issue in j_issues:
     comments += j_issue["comments"]["totalCount"]
-print(f"Start to translate {len(j_issues)} issues, {comments} comments.")
+print(f"Start to translate {len(j_issues)} objects, {comments} comments.")
 
 for index, j_issue in enumerate(j_issues):
     print("\n\n\n\n\n\n")
-    print(f"===============Issue(#{index+1})===============")
+    print(f"===============Object(#{index+1})===============")
     print(f"ID: {j_issue['id']}")
     print(f"Title: {j_issue['title']}")
     print(f"URL: {j_issue['url']}")
--- srs/pr-refine.sh
+++ srs/pr-refine.sh
@@ -12,6 +12,8 @@ input=
 token=
 proxy=
 key=
+v5=
+v6=
 
 while [[ "$#" -gt 0 ]]; do
     case $1 in
@@ -20,6 +22,8 @@ while [[ "$#" -gt 0 ]]; do
         --token) token=$2; shift 2;;
         --proxy) proxy=$2; shift 2;;
         --key) key=$2; shift 2;;
+        --v5) v5=$2; shift 2;;
+        --v6) v6=$2; shift 2;;
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
 done
@@ -29,6 +33,8 @@ if [[ "$help" == yes ]]; then
     echo "Options:"
     echo "  -h, --help    Show this help message and exit"
     echo "  --input       The url of pr, for example, GitHub PR URL, for example, https://github.com/your-org/your-repository/pull/3699"
+    echo "  --v5          Whether merge to v5 branch"
+    echo "  --v6          Whether merge to v6 branch"
     echo "  --token       (Optional) GitHub access token, for example, github_pat_xxx_yyyyyy"
     echo "  --proxy       (Optional) OpenAI API proxy, for example, x.y.z"
     echo "  --key         (Optional) OpenAI API key, for example, xxxyyyzzz"
@@ -37,6 +43,11 @@ fi
 
 if [[ -z $input ]]; then
     echo "Error: --input is empty"
+    exit 1
+fi
+
+if [[ -z $v5 && -z $v6 ]]; then
+    echo "Error: --v5 and --v6 are empty"
     exit 1
 fi
 
@@ -54,5 +65,35 @@ if [[ ! -z $key ]]; then
     PARAMS="$PARAMS --key $key"
 fi
 
+echo -e "\n\n\n\n\n\n"
+echo "========================================================================"
+echo "==== python pr-trans.py $PARAMS"
+echo "========================================================================"
 python pr-trans.py $PARAMS
 ret=$?; if [[ $ret != 0 ]]; then echo "Translate pr failed"; exit $ret; fi
+
+echo -e "\n\n\n\n\n\n"
+echo "========================================================================"
+echo "==== python pr-rephrase.py $PARAMS"
+echo "========================================================================"
+python pr-rephrase.py $PARAMS
+ret=$?; if [[ $ret != 0 ]]; then echo "Rephrase pr failed"; exit $ret; fi
+
+if [[ ! -z $v5 ]]; then
+    PARAMS="$PARAMS --v5 $v5"
+fi
+if [[ ! -z $v6 ]]; then
+    PARAMS="$PARAMS --v6 $v6"
+fi
+echo -e "\n\n\n\n\n\n"
+echo "========================================================================"
+echo "==== python pr-release-update.py $PARAMS"
+echo "========================================================================"
+python pr-release-update.py $PARAMS
+ret=$?; if [[ $ret != 0 ]]; then echo "Update release of pr failed"; exit $ret; fi
+
+
+echo -e "\n\n\n\n\n\n"
+echo "========================================================================"
+echo "==== OK!"
+echo "========================================================================"
