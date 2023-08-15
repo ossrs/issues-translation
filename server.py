2c56e6bca08bf016e1721543452e4af3dea66b96
winlin <winlin@vip.126.com>
2023-08-15 20:41:16 +0800

    Update

--- server.py
+++ server.py
@@ -234,7 +234,8 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         content_length = int(self.headers.get('Content-Length'))
         req_body = self.rfile.read(content_length)
         j_req = json.loads(req_body.decode('utf-8'))
-        print(f"Got a request {self.path} {len(req_body)}B {self.headers}")
+        #print(f"Got a request {self.path} {len(req_body)}B {self.headers}")
+        print(f"Got a request {self.path} {self.headers} {req_body}")
 
         headers = {}
         for key in self.headers.keys():
