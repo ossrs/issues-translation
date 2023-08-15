403bc9a0632972f9b054c33fa044a3fe86aec06d
winlin <winlin@vip.126.com>
2023-08-15 20:48:58 +0800

    Update

--- server.py
+++ server.py
@@ -45,7 +45,7 @@ def handle_oc_request(j_req, event, delivery, headers):
             del headers['content-type']
         headers['Content-Type'] = 'application/json'
         res = requests.post(args.open_collective, json=j_req, headers=headers)
-        print(f"Thread: {delivery}: Response {res.status_code} {res.reason} {len(res.text)}B {res.headers}")
+        print(f"Thread: {delivery}: Response {res.status_code} {res.reason} {res.text} {res.headers}")
 
     print(f"Thread: {delivery}: Done")
 
